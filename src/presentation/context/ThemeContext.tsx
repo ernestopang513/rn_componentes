5f1de142cc57316f7344f4e1d7bfbd5f47db57e6
Ernesto Pang <ernestopang513@hotmail.com>
2025-01-14 18:51:59 -0600

    Sincronización con tema del sistema operativo

--- src/presentation/context/ThemeContext.tsx
+++ src/presentation/context/ThemeContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, PropsWithChildren, useEffect, useState } from 'react';
 import { darkColors, darkRedColors, lightColors, ThemeColors } from "../../config/theme/theme";
-import { Appearance, AppState, useColorScheme } from 'react-native';
+import { AppState, useColorScheme } from 'react-native';
 
 type ThemeColor = 'light' | 'dark' | 'redDark';
 
@@ -24,21 +24,23 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 export const ThemeProvider = ({children}: PropsWithChildren) => {
 
     const colorScheme = useColorScheme();
+
     const [currentTheme, setCurrentTheme] = useState<ThemeColor>('light')
 
-    // useEffect(() => {
-    //     // console.log(colorScheme)
-    //     if (colorScheme === 'dark') {
-    //         setCurrentTheme('dark')
-    //     } else {
-    //         setCurrentTheme('light')
-    //     }
-    // }, [colorScheme])
+    useEffect(() => {
+        if (colorScheme === 'dark') {
+            setCurrentTheme('dark')
+        } else {
+            setCurrentTheme('light')
+        }
+    }, [colorScheme])
 
     useEffect(()=>{
+        console.log('me ejecuto use effect')
         const subscription = AppState.addEventListener('change' , nextAppState => {
-            const colorScheme = Appearance.getColorScheme();
-            setCurrentTheme(colorScheme === 'dark' ? 'dark' : 'light')
+            console.log('Actualmente estoy en: ',{AppState})
+            console.log('La app se fue a background')
+            console.log({nextAppState})
         })
 
         return () => {
